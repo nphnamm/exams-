08ace7e79e03ef72f381336c35a0c6076ae5a4b8
nphnamm <nphnamm@gmail.com>
2024-01-16 01:34:27 +0700

    new code

---
+++ DinhThiKimThoa_De7/DinhThiKimThoa_De7/Controllers/PhanLoaisController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using DinhThiKimThoa_De7.Models;
+
+namespace DinhThiKimThoa_De7.Controllers
+{
+    public class PhanLoaisController : Controller
+    {
+        private QLBanChauCanhEntities db = new QLBanChauCanhEntities();
+
+        // GET: PhanLoais
+        public async Task <ActionResult> Index()
+        {
+            
+            var sanpham = await db.SanPhams.ToListAsync();
+            ViewBag.sanpham = sanpham;
+            return View(await db.PhanLoais.ToListAsync());
+        }
+
+        // GET: PhanLoais/Details/5
+        public ActionResult Details(string id)
+        {
+            
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhanLoai phanLoai = db.PhanLoais.Find(id);
+            if (phanLoai == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phanLoai);
+        }
+
+        // GET: PhanLoais/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PhanLoais/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaPhanLoai,PhanLoaiChinh")] PhanLoai phanLoai)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PhanLoais.Add(phanLoai);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(phanLoai);
+        }
+
+        // GET: PhanLoais/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhanLoai phanLoai = db.PhanLoais.Find(id);
+            if (phanLoai == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phanLoai);
+        }
+
+        // POST: PhanLoais/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaPhanLoai,PhanLoaiChinh")] PhanLoai phanLoai)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(phanLoai).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(phanLoai);
+        }
+
+        // GET: PhanLoais/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhanLoai phanLoai = db.PhanLoais.Find(id);
+            if (phanLoai == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phanLoai);
+        }
+
+        // POST: PhanLoais/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            PhanLoai phanLoai = db.PhanLoais.Find(id);
+            db.PhanLoais.Remove(phanLoai);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult GetProductByCategory(string phanloai)
+        {
+
+            var sanPham = db.SanPhams
+                .Where(sp => sp.PhanLoai.PhanLoaiChinh == phanloai)
+                .ToList();
+
+            var _sanPham = sanPham
+                .Select(sp => new SanPham
+                {
+                    MaSanPham = sp.MaSanPham,
+                    TenSanPham = sp.TenSanPham,
+                    AnhDaiDien = sp.AnhDaiDien,
+                    DonGiaBanNhoNhat = sp.DonGiaBanNhoNhat,
+                }).ToList();
+
+            return Json(new { sanPham = _sanPham }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ demot/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using demot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demot.Controllers
+{
+    public class HomeController : Controller
+    {
+        private QLBanHangQuanAoEntities db = new QLBanHangQuanAoEntities();
+        public ActionResult Index()
+        {
+            var Phanloai = db.PhanLoais.ToList();
+            ViewBag.Phanloai = Phanloai;
+            var Sanpham = db.SanPhams.ToList();
+            ViewBag.Sanpham = Sanpham;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult GetProductByCategory(string phanloai)
+        {
+            if (phanloai == "Tất cả sản phẩm")
+            {
+                var sanPham1 = db.SanPhams.ToList();
+
+                var _sanPham1 = sanPham1
+                .Select(sp => new SanPham
+                {
+                    MaSanPham = sp.MaSanPham,
+                    TenSanPham = sp.TenSanPham,
+                    DonGiaBanNhoNhat = sp.DonGiaBanNhoNhat,
+                    DonGiaBanLonNhat = sp.DonGiaBanLonNhat,
+                    TrangThai = sp.TrangThai,
+                    MoTaNgan = sp.MoTaNgan,
+                    AnhDaiDien = sp.AnhDaiDien,
+                    NoiBat = sp.NoiBat,
+                    MaPhanLoaiPhu = sp.MaPhanLoaiPhu,
+                    MaPhanLoai = sp.MaPhanLoai,
+                    GiaNhap = sp.GiaNhap
+                }).ToList();
+                return Json(new { sanPham = _sanPham1 }, JsonRequestBehavior.AllowGet);
+            }
+            var sanPham = db.SanPhams
+                .Where(sp => sp.PhanLoai.PhanLoaiChinh == phanloai)
+                .ToList();
+
+            // tạo danh sách mới chỉ lấy các thuộc tính
+            // không lấy các đối tượng tham chiếu
+            var _sanPham = sanPham
+                .Select(sp => new SanPham
+                {
+                    MaSanPham = sp.MaSanPham,
+                    TenSanPham = sp.TenSanPham,
+                    GiaNhap = sp.GiaNhap,
+                    AnhDaiDien = sp.AnhDaiDien
+
+                }).ToList();
+
+            return Json(new { sanPham = _sanPham }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
